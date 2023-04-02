786ee446faba20500fd6375ee99e70657917d514
David Card <davidcardjr@gmail.com>
2023-04-02 11:44:43 -0400

    Change to Hey CodeCrew in Cloned project

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("What's up CodeCrew?")
+            Text("Hey CodeCrew?")
         }
         .padding()
     }
